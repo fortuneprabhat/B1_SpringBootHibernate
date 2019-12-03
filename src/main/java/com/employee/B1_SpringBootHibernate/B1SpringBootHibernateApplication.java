997d79f9d00830f81fe30ba36431a5f5fb8a60c1
Prabhat Kumar <prabhat.kumar05@nagarro.com>
2019-12-03 12:31:31 +0530

    This is an Initial commit for the project.

---
+++ src/main/java/com/employee/B1_SpringBootHibernate/B1SpringBootHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.employee.B1_SpringBootHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B1SpringBootHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B1SpringBootHibernateApplication.class, args);
+	}
+
+}
